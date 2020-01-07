f860182334c60accf7fdf8a267896811c7caceeb
Vishal Antony <viantony@cisco.com>
2020-01-07 11:46:11 +0530

    Adding constant time solution to Problem1

--- src/main/scala/projectEuler/Problem1.scala
+++ src/main/scala/projectEuler/Problem1.scala
@@ -5,8 +5,18 @@ object Problem1 extends App {
   def getAllMultiplesOf3or5(below: Int): Seq[Int] =
     (1 until below).filter(x => (x % 3 == 0) || (x % 5 == 0))
 
-  def getSumOfAllMultiplesOf3or5(below: Int): Int =
+  // Brute force solution
+  def getSumOfAllMultiplesOf3or5_bruteForce(below: Int): Int =
     getAllMultiplesOf3or5(below).sum
+
+  def sumOfMultiplesTill(till: Int)(n: Int): Int =
+    n * (till/n) * (till/n + 1) / 2
+
+  // Constant time solution
+  def getSumOfAllMultiplesOf3or5(below: Int): Int = {
+    val sumOfMultiplesOf = sumOfMultiplesTill(below - 1)(_)
+    sumOfMultiplesOf(3) + sumOfMultiplesOf(5) - sumOfMultiplesOf(15)
+  }
 
   println(getSumOfAllMultiplesOf3or5(1000))
 }
